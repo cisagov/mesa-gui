8c159379907c907d3511cbecdb648912684ef8c7
Doug Foster <105928061+CyberDoug@users.noreply.github.com>
2024-06-13 09:14:15 -0500

    Added sslserver package and ssl settings

--- mesa_gui/mesa_gui/settings.py
+++ mesa_gui/mesa_gui/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mesa',
+    'sslserver'
 ]
 
 MIDDLEWARE = [
@@ -129,3 +130,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SECURE_SSL_REDIRECT = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
